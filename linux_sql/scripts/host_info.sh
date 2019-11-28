3d9ab85dd33697efd9145ea1f3dcb896f7c3f6e1
james <jamesli9572@gmail.com>
2019-11-28 22:34:10 +0000

    apply DRY principle

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -9,20 +9,24 @@ psql_password=$5
 
 lscpu_out=`lscpu`
 
+#helper function to eliminate duplicate code
+parse_helper(){
+    echo $(echo "$lscpu_out"  | egrep "$1" | awk -F':' '{print $2}' | xargs)
+}
 #parse data and setup variable
 hostname=$(hostname -f)
-cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(echo "$lscpu_out"  | egrep "Architecture:" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_out"  | egrep "Model name:" | awk '{$1=$2=""; print $0}' | xargs)
-cpu_mhz=$(echo "$lscpu_out"  | egrep "CPU MHz:" | awk '{print $3}' | xargs)
+cpu_number="$(parse_helper "^CPU\(s\):")"
+cpu_architecture="$(parse_helper "Architecture")"
+cpu_model="$(parse_helper "Model name:")"
+cpu_mhz="$(parse_helper "CPU MHz:")"
 L2_cache=$(echo "$lscpu_out"  | egrep "L2 cache:" | awk '{print $3}' | sed s'/K//' | xargs)
 total_mem=$(vmstat --unit M | tail -1 | awk '{print $4}')
 timestamp=$(date --rfc-3339=seconds | awk -F+ '{print $1}' | xargs)
 
 #constuct insert statement
 insert_statement="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, "timestamp") VALUES ('${hostname}', ${cpu_number}, '${cpu_architecture}', '${cpu_model}', ${cpu_mhz}, ${L2_cache}, ${total_mem}, '${timestamp}');"
-#echo $insert_statement
+echo $insert_statement
 
 #execute insert statement
-export PGPASSWORD=$psql_password
-psql -h $psql_host -p $psql_port -U $psql_user -d $db_name -c "$insert_statement"
+#export PGPASSWORD=$psql_password
+#psql -h $psql_host -p $psql_port -U $psql_user -d $db_name -c "$insert_statement"
