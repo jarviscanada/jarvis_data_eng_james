73328237d403b6d2d69d4313d738efd68b1f281a
james <jamesli9572@gmail.com>
2019-11-29 22:54:08 +0000

    adding the second query

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,15 +1,32 @@
 --Query 1
 --Group hosts by CPU number and sort by their memory size in descending order
 --(within each cpu_number group)
-select
+SELECT
   info.cpu_number,
   host.id AS host_id,
   info.total_mem
-from
+FROM
   host_info AS info
-ORDER BY 
+ORDER BY
   info.total_mem DESC;
 
 --Query 2
 -- Average used memory in percentage over 5 mins interval for each host.
 --(used memory = total memory - free memory).
+SELECT
+  usage.host_id,
+  info.hostname AS host_name,
+  info.total_mem AS total_memory,
+  AVG((info.total_mem - usage.memory_free) / info.total_mem * 100)::INT AS used_memory_percentage
+FROM
+  host_usage AS usage
+  JOIN
+  host_info AS info
+  ON usage.host_id=info.id
+GROUP BY
+  usage.host_id,
+  info.hostname,
+  info.total_mem,
+  DATE_TRUNC('hour', usage."timestamp") + interval '5 MINUTE' * (DATE_PART('MINUTE',usage."timestamp")/5.0)::int
+ORDER BY
+  usage.host_id;
