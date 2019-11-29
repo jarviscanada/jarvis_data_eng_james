fd059fcf39d817d2783e6ceb16a7459f3abddf60
james <jamesli9572@gmail.com>
2019-11-29 20:46:40 +0000

    modify query1

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,9 +1,14 @@
 --Query 1
 --Group hosts by CPU number and sort by their memory size in descending order
 --(within each cpu_number group)
-select info.cpu_number, usuage.host_id, info.total_mem
-from host_info as info join host_usage as usuage on info.id = usage.host_id
-ORDER BY info.total_mem DESC;
+select
+  info.cpu_number,
+  host.id AS host_id,
+  info.total_mem
+from
+  host_info AS info
+ORDER BY 
+  info.total_mem DESC;
 
 --Query 2
 -- Average used memory in percentage over 5 mins interval for each host.
